1d20c34f0c4292529cb45fae27b5f5299886f9f0
WojciechK2 <klasa.1.wojciech@gmail.com>
2023-01-13 20:19:19 +0100

    cleaning up views

---
+++ ViewModels/ObjectViewModels/CommonObjectViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cosmos.ViewModels.ObjectViewModels
+{
+    //a class for all of the "*ObjectViewModel" to inherit (type generalization)
+    public class CommonObjectViewModel
+    {
+    }
+}
