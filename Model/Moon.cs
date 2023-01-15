8523c2a9594ef197ee08103251dbd88112419559
WojciechK2 <klasa.1.wojciech@gmail.com>
2023-01-15 16:40:52 +0100

    Restructuring

---
+++ Model/Moon.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cosmos
+{
+    public class Moon : AstronomicalObject
+    {
+        public UnitValue<long> DistanceFromPlanet { get; set; }
+
+        public Moon(
+            string name,
+            UnitValue<long> size,
+            UnitValue<double> mass,
+            UnitValue<long> distanceFromEarth,
+            UnitValue<double> apparentMagnitude,
+            Declination declination,
+            RightAscension rightAscension,
+            UnitValue<long> distanceFromPlanet) : base(name, size, mass, distanceFromEarth, apparentMagnitude, declination, rightAscension)
+        {
+            DistanceFromPlanet = distanceFromPlanet;
+        }
+    }
+}
---
+++ Model/PlanetarySystem.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cosmos
+{
+    public class PlanetarySystem : BasicAstronomicalObject
+    {
+        public List<Planet> Planets { get; set; } = new List<Planet>();
+
+        public PlanetarySystem(
+            string name,
+            UnitValue<long> size,
+            UnitValue<long> distanceFromEarth,
+            UnitValue<double> apparentMagnitude,
+            Declination declination,
+            RightAscension rightAscension) : base(name, size, distanceFromEarth, apparentMagnitude, declination, rightAscension)
+        {
+
+        }
+
+        public Planet GetPlanet(long id)
+        {
+            return FindPlanetById(id);
+        }
+
+        public void AddPlanet(Planet planet)
+        {
+            Planets.Add(planet);
+        }
+
+        public List<Planet> GetPlanets()
+        {
+            return Planets;
+        }
+
+        public void RemovePlanet(long id)
+        {
+            Planets.Remove(FindPlanetById(id));
+        }
+
+        private Planet FindPlanetById(long id)
+        {
+            return Planets.Find(p => p.Id == id);
+        }
+    }
+}
